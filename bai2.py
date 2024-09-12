7bcc4d5c3c6fdef6c5fd7d8b0e2b1d03c5a63311
poormyselfx3000 <vipchivaythoi3@gmail.com>
2024-09-12 10:13:44 -0700

    suadung

--- bai2.py
+++ bai2.py
@@ -1,18 +1,28 @@
 # Cho xâu S tối đa 255 ký tự gồm các chữ cái, chữ số và dấu cách. 
 # Hãy đếm xem xâu S có bao nhiêu từ (một từ là một hoặc nhiều kí tự viết liền nhau).
 
-s = str(input("hay nhap xau S (toi da 255 ky tu): "))
-if len(s) > 255 :
-    print("da qua 255 ky tu roi")
-else:    
+def tinh_so_tu(chuoi):
     ktr_xau = []
     tempoftemp = ""
-    for temp in s:
+    for temp in chuoi:
         if temp == " ":
-            ktr_xau.append(tempoftemp)
-            tempoftemp = ''
+            if tempoftemp != "":
+                ktr_xau.append(tempoftemp)
+                tempoftemp = ''
         else:
             tempoftemp += temp
-
-    ktr_xau.append(tempoftemp)
+    if tempoftemp != "":
+        ktr_xau.append(tempoftemp)
     print(len(ktr_xau))
+
+
+s = str(input("hay nhap xau S (toi da 255 ky tu): "))
+
+
+if len(s) > 255 :
+    print("da qua 255 ky tu roi")
+
+else:
+    tinh_so_tu(s)
+
+
